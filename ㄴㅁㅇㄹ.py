8fa1df25732de3ea28e90f57b7481b87fd0599ed
syk-genon <yongki.sim@genon.ai>
2026-02-09 16:44:40 +0900

    Update ㄴㅁㅇㄹ.py

--- ㄴㅁㅇㄹ.py
+++ ㄴㅁㅇㄹ.py
@@ -1,7 +1,22 @@
-python - << 'EOF'
-import sys
-print("sys.executable:", sys.executable)
-print("site-packages:", sys.path)
-import pip
-print("pip path:", pip.__file__)
-EOF
+import asyncio
+from temporalio import worker
+from temporalio.client import Client
+
+from workflow import LawCrawlWorkflow
+from activity import crawl_law_page
+
+async def main():
+    client = await Client.connect("localhost:7233")
+
+    w = worker.Worker(
+        client,
+        task_queue="LAW_CRAWL_QUEUE",
+        workflows=[LawCrawlWorkflow],
+        activities=[crawl_law_page],
+    )
+
+    print("Worker started: LAW_CRAWL_QUEUE")
+    await w.run()
+
+if __name__ == "__main__":
+    asyncio.run(main())
